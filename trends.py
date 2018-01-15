bbeeb59469d3cc630b273d1408c282c820542d93
RuxandraV <Ruxandra.Vulpoiu@ortec-finance.com>
2018-01-15 16:26:30 +0100

    started iteration 2 of experiments: data trends

---
+++ trends.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+##### upload data
+data_nov = pd.read_csv('../Data/model_data.csv', sep=';', header = 0);
+data_dec = pd.read_csv('../Data/model_data2.csv', sep=';', header = 0);
+data_jan = pd.read_csv('../Data/model_data3.csv', sep=';', header = 0);
+
